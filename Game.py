d06f56e28e604a3c8e259c2727c5dcc5b93b8958
Richard Hildred <rhildred@gmail.com>
2018-11-20 22:46:26 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,5 +2,6 @@ class Game:
     def __init__(self):
         self.__nCurrent = 0
     def takeTurn(self, sPhone, sInput):
-        return[sInput + " sms from " + sPhone + " answered here"]
+        self.__nCurrent += 1
+        return[sInput + " sms from " + sPhone + " answered here " + str(self.__nCurrent)]
 
