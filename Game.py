ea7886fb79e1e6d774c8f60a4ee8261cf41c1e47
Richard Hildred <rhildred@gmail.com>
2018-11-22 20:41:50 -0500

    made this a "real" game

--- Game.py
+++ Game.py
@@ -1,7 +1,39 @@
+import random
+
 class Game:
     def __init__(self):
-        self.__nCurrent = 0
+        self.__nCurrent = -1
     def takeTurn(self, sPhone, sInput):
-        self.__nCurrent += 1
-        return[sInput + " sms from " + sPhone + " answered here " + str(self.__nCurrent)]
-
+        if(self.__nCurrent == -1):
+            self.__nCurrent = 0
+            return["Welcome to 20 + 1",
+                   "in this game the object is to make the computer count 21",
+                   "you count from 1-4 steps on the way to 21"
+                ]
+        else:
+            aNumbers = sInput.split(",")
+            for n in aNumbers:
+                self.__nCurrent += 1
+            if self.__nCurrent > 15:
+                if self.__nCurrent == 20:
+                    self.__nCurrent = -1
+                    return["21", "you win"]
+                else:
+                    sNumbers = ""
+                    while self.__nCurrent < 20:
+                        if sNumbers != "":
+                            sNumbers += ", "
+                        self.__nCurrent += 1
+                        sNumbers += str(self.__nCurrent)
+                    self.__nCurrent = -1
+                    return[sNumbers, "I win ... ha"]
+            else:
+                nCount = random.randint(1,4)
+                sNumbers = ""
+                for n in range(0, nCount):
+                    if sNumbers != "":
+                        sNumbers += ", "
+                    self.__nCurrent += 1
+                    sNumbers += str(self.__nCurrent)
+                return[sNumbers]
+                
