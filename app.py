6fa19333007731f9598ccaf9b27c53266c1163e1
Rich Hildred <rhildred@gmail.com>
2019-11-19 17:26:36 +0000

    added process control where it kills the process bound to the port if already in use.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import Response
 from flask import request
 from Game import Game
+from psutil import process_iter
+from signal import SIGTERM # or SIGKILL
 
 app = Flask(__name__)
 
@@ -27,4 +29,10 @@ def server():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    nPort = 8080
+
+    for proc in process_iter():
+        for conns in proc.connections(kind='inet'):
+            if conns.laddr.port == nPort:
+                proc.send_signal(SIGTERM) # or SIGKILL
+    app.run(host='0.0.0.0', port=nPort)
--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 import subprocess
 subprocess.check_call(["python3", '-m', 'pip', 'install', 'Flask']) # install Flask
+subprocess.check_call(["python3", '-m', 'pip', 'install', 'psutil']) # install psutil for process management
