25cd7a9b65f358715b2d84759937b9c1b98a74b3
Richard Hildred <rhildred@gmail.com>
2021-11-25 16:12:55 -0500

    removed the piece that kills previous games

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import render_template
 from flask import Response
 from flask import request
 from Game import Game
-from psutil import process_iter
-from signal import SIGTERM # or SIGKILL
 
 app = Flask(__name__)
 
@@ -31,8 +29,4 @@ def server():
 if __name__ == '__main__':
     nPort = 8080
 
-    for proc in process_iter():
-        for conns in proc.connections(kind='inet'):
-            if conns.laddr.port == nPort:
-                proc.send_signal(SIGTERM) # or SIGKILL
     app.run(host='0.0.0.0', port=nPort)
