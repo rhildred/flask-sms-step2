c415a9a7fd790e5a610dcaac11b2386683efa155
Richard Hildred <rhildred@gmail.com>
2021-11-25 16:32:49 -0500

    remove psutil

--- setup.py
+++ setup.py
@@ -1,3 +1,2 @@
 import subprocess
 subprocess.check_call(["python3", '-m', 'pip', 'install', 'Flask']) # install Flask
-subprocess.check_call(["python3", '-m', 'pip', 'install', 'psutil']) # install psutil for process management
